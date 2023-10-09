93935d6850ab18493af17ae500e69059939fd25c
Usuario Local <artemvar@ucm.es>
2023-10-09 19:39:47 +0200

    copypaste siuuu

--- ochoreinas/main.py
+++ ochoreinas/main.py
@@ -11,3 +11,40 @@
 # las reinas están en (x1, y1), (x7, y7)...
 # Si las posiciones son invalidas en ambos casos habria un -1
 # para ver q se ha alcanzado podemos o tener un contador en el estado, o ver si no hay ningun -1 en el estado
+# import time
+# time() - returns current datetime
+# 4 decimales de precision
+# ALGORITMO   N   TIME
+# BPA        8    0.02
+from ochoreinas import OchoReinas
+
+# 1
+state = (-1,-1,-1,-1,-1,-1,-1,-1)
+ep = OchoReinas(state)
+
+"""
+Empezamos con el estado (-1,-1,-1,-1), tablero vacio 
+Nos vamos a mover columna por columna.
+Es decir, elegimos una fila para la reina y cambiamos de columna(siguiente reina)
+(-1,-1,-1,-1)
+        (0,-1,-1,-1)                    
+            (0, 1,-1,-1)
+            (0, 2,-1,-1)
+                (0, 2, 1,-1)
+                (0, 2, 3,-1)
+            (0, 3,-1,-1)
+        (1,-1,-1,-1)   
+            (1, 0,-1,-1)
+            (1, 2,-1,-1)
+            (1, 3,-1,-1)              
+        (2,-1,-1,-1)  
+            (2, 0,-1,-1)
+            (2, 1,-1,-1)
+            (2, 3,-1,-1)                     
+        (3,-1,-1,-1)
+            (3, 0,-1,-1)
+            (3, 1,-1,-1)
+            (3, 2,-1,-1)
+    
+
+"""
--- ochoreinas/ochoreinas.py
+++ ochoreinas/ochoreinas.py
@@ -4,15 +4,23 @@ class OchoReinas(Problem):
     # mejor empezar con 4x4, no 8x8
     def __init__(self, initial):
         super().__init__(initial)
+
     def actions(self, state):
-        # acciones posibles
-        return []
+        """In the leftmost empty column, try all non-conflicting rows."""
+        if state[-1] != -1:
+            return []  # All columns filled; no successors
+        else:
+            col = state.index(-1)
+            return [row for row in range(8)
+                    if not self.conflicted(state, row, col)]
+
+    def conflicted(self, state, row, col):
+        """Would placing a queen at (row, col) conflict with anything?"""
+        return any(self.conflict(row, col, state[c], c)
+                   for c in range(col))
 
     def result(self, state, action):
-        # action es tupla (fila, col)
-        state[state[0] + 1] = action[0]
-        state[state[0] + 2] = action[1]
-        state[0]+=1
+
         return None
 
     def goal_test(self, state):
