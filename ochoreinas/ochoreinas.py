3ac856692c12465464143ba6ed9169a9afb27064
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-10-23 19:53:34 +0200

    Update ochoreinas.py

--- ochoreinas/ochoreinas.py
+++ ochoreinas/ochoreinas.py
@@ -3,16 +3,16 @@ from search import Problem
 
 class OchoReinas(Problem):
     tam = 8 # el tablero es 8x8
-    def __init__(self, initial):
-        super().__init__(initial)
+    def __init__(self):
+        super().__init__([0, list([-1])*self.tam, list([False])*(self.tam*2-1), list([False])*(self.tam*2-1)])
 
     def actions(self, state):
         """Devuelve las filas donde se puede poner la reina, 
         para la primera colmna vacia, empezando por el lado izquierdo """
-        if state[-1] != -1:
+        if state[0] == self.tam:
             return []  # Todas las columnas tienen asignadas una reina
         else:
-            col = state.index(-1)
+            col = state[0]
             availableRows = []
             for row in range(self.tam):
                 if not self.conflicted(state, row, col):
@@ -22,31 +22,30 @@ class OchoReinas(Problem):
     def conflicted(self, state, row, col):
         """Devuelve true si hay alguna reina que puede comer a la reina en esta posicion (row, col)"""
         # no hace falta revisar columnas
-        conflict = False
-        i = 0
-        while (not conflict and i < col):
-            if (state[i] == row or abs((i-col)/(state[i]-row)) == 1): # misma fila o misma diagonal
-                conflict = True
-            i+=1
-
-        return conflict
+        return not (state[1][row] == -1 and not state[2][row + col] and not state[3][(self.tam - 1) - row + col])
 
     def result(self, state, action):
         """Aplica al estado la accion correspondiente"""
-        if -1 in state:
-            col = state.index(-1)
-            # Como la tupla es inmutable, tenemos que convertirla a lista
-            # src: https://www.w3schools.com/python/gloss_python_change_tuple_item.asp#:~:text=Once%20a%20tuple%20is%20created,as%20it%20also%20is%20called.
-            state = list(state)
-            state[col] = action
-            state = tuple(state)
-        return state
+        # state[0] es la columna en la que colocaremos la reina
+        # action es la fila en la que colocaremos la reina
+        # ponemos una reina en (action, state[0])
+        newState = [state[0]]# copia de lista
+        newState.append(state[1].copy())
+        newState.append(state[2].copy())
+        newState.append(state[3].copy())
+        col = newState[0]
+        row = action
+        newState[1][row] = col
+        newState[2][row + col] = True # marcamos la diagonal, pasa a tener una reina
+        newState[3][(self.tam - 1) - row + col] = True # marcamos la diagonal, pasa a tener una reina
+        newState[0] += 1
+        return newState
 
     def goal_test(self, state):
         # PRE: State es valido, reinas no se comen entre si
-        if state[-1] == -1:  # si la ultima columna no esta rellenada no es valida
-            return False
-        return True
+        if state[0] == self.tam:# """and state[1] != [0, 6, 4, 7, 1, 3, 5, 2]"""   En todas las columnas hay una reina
+            return True
+        return False
 
 
 
