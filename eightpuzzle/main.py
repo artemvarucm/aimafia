92c7b5c7fd110f67fbee30ef7ebe2d2a213c6108
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-10-23 19:52:55 +0200

    Update main.py

--- eightpuzzle/main.py
+++ eightpuzzle/main.py
@@ -6,12 +6,16 @@ state = (2,5,6,0,3,8,4,1,7)
 ep = EightPuzzle(state)
 print(ep.initial) # devuelve la tupla del estado inicial
 # resultado (2,5,6,0,3,8,4,1,7)
+
 # 2
 # devuelve acciones posibles a realizar(adonde se puede mover el "agujero")
 posibles = ep.actions(ep.initial)
+print(posibles)
+
 # Para ejecutar las acciones necesitamos ejecutar la funcion result
 # 3
 print(ep.goal_test(ep.initial))
 # 4
-ep.result(state, "UP")
+state = ep.result(state, "UP")
 print(ep.goal_test(state))
+print(list([-1])*8)
