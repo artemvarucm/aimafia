ef5ce579f8b5551610d9a25297cb2dbe07fd262a
Artem <artemvar@ucm.es>
2023-10-24 12:21:19 +0200

    Parte 1 acabada

--- ochoreinas/main.py
+++ ochoreinas/main.py
@@ -1,94 +1,13 @@
 # Ejercicio 2
 
 # Ocho reinas
-# Como guardar estado -> necesitamos guardar las posiciones y cuantas reinas hay
-# dos posibles opciones para estado
-# uno, como esta en search.py, usar la posicion de la tupla como columna
-# si tenemos el siguiente estado (N, x1, x9, x3, x4, x5, x49, x7, x8)
-# las reinas están en (x1, 0), (x9, 1), (x49, 5)...
-# dos, como mas sencillo, pero menos eficiente
-# si tenemos el siguiente estado (N, x1, y1, x7, y7...)
-# las reinas están en (x1, y1), (x7, y7)...
-# Si las posiciones son invalidas en ambos casos habria un -1
-# para ver q se ha alcanzado podemos o tener un contador en el estado, o ver si no hay ningun -1 en el estado
-# import time
-# time() - returns current datetime
-# 4 decimales de precision
-# ALGORITMO   N   TIME
-# BPA        8    0.02
 from ochoreinas import OchoReinas
 from search import breadth_first_tree_search, depth_first_tree_search, depth_limited_search, iterative_deepening_search
 
-# 1
+# Apartado d
 ep = OchoReinas()
-for action in ep.actions(ep.initial):
-    print(ep.result(ep.initial, action))
-
-"""
-REVIEW
-
-Empezamos con el estado (-1,-1,-1,-1), tablero vacio 
-Nos vamos a mover columna por columna.
-Es decir, elegimos una fila para la reina y avanzamos de columna(siguiente reina)
-(-1,-1,-1,-1)
-        (0,-1,-1,-1)                    
-            (0, 2,-1,-1)
-                (0, 2, 1,-1)
-                (0, 2, 3,-1)
-            (0, 3,-1,-1)
-        (1,-1,-1,-1)   
-            (1, 3,-1,-1)              
-        (2,-1,-1,-1)  
-            (2, 0,-1,-1)                    
-        (3,-1,-1,-1)
-            (3, 0,-1,-1)
-
-MÉTODO ACTION
-pre-condicion
-tablero valido(reinas no se pueden comer entre si)
-post-condicion
-devuelve la lista de las filas validas(donde no se puede comer) posibles para la columna actual 
-
-
-
-Comprobar objetivo -> llegamos a un estado sin "-1"s
-
-Coste de cada acción O(n^2) respecto a n = número de filas = 8
-
-"""
-
-#####
-    #
-#####
-    #
-#####
-def imprimirSolucion(solNode, algoritmo):
-    print()
-    print(f'Algoritmo {algoritmo}')
-    if solNode != None:
-        state = solNode.state[1]
-        
-        print("-"*22)
-        print(f'{"Reina":<9}|{"Fila":>5}|{"Col":>5}|')
-        print("-"*22)
-        # FIXME: imprimir por filas ordenadamente
-        # state[i] es la fila, i es la columna
-        for i in range(len(state)):
-            print(f'{f"Reina {i+1}":<9}|{i:>5.0f}|{state[i]:>5.0f}|')
-            print("-"*22)
-    else:
-        print("No ha encontrado solucion")
-
-
-# anadir limite de 30 minutos
-ep1 = OchoReinas()
-solucionBPA = breadth_first_tree_search(ep1)
-imprimirSolucion(solucionBPA, "BPA")
-solucionBPP = depth_first_tree_search(ep1)
-imprimirSolucion(solucionBPA, "BPP")
-solucionBPL = depth_limited_search(ep1)
-imprimirSolucion(solucionBPL, "BPL")
-solucionBPI = iterative_deepening_search(ep1)
-imprimirSolucion(solucionBPI, "BPI")
+for action1 in ep.actions(ep.initial):
+    state1 = ep.result(ep.initial, action1)
+    print(state1, "Es objectivo: ", ep.goal_test(state1))
 
 
--- ochoreinas/ochoreinas.py
+++ ochoreinas/ochoreinas.py
@@ -9,6 +9,8 @@ class OchoReinas(Problem):
     def actions(self, state):
         """Devuelve las filas donde se puede poner la reina, 
         para la primera colmna vacia, empezando por el lado izquierdo """
+        # Coste lineal O(N) en el peor caso, donde N = tamanio, hay un bucle donde cada iteracion es de coste constante y se repite n veces
+        # O(1 * N) = O(N) 
         if state[0] == self.tam:
             return []  # Todas las columnas tienen asignadas una reina
         else:
@@ -21,15 +23,18 @@ class OchoReinas(Problem):
 
     def conflicted(self, state, row, col):
         """Devuelve true si hay alguna reina que puede comer a la reina en esta posicion (row, col)"""
+        # Coste constante O(1) - acceso a la posición de la lista del estado
         # no hace falta revisar columnas
         return not (state[1][row] == -1 and not state[2][row + col] and not state[3][(self.tam - 1) - row + col])
 
     def result(self, state, action):
+        # Coste constante O(1) - operaciones de asignación simples
         """Aplica al estado la accion correspondiente"""
         # state[0] es la columna en la que colocaremos la reina
         # action es la fila en la que colocaremos la reina
         # ponemos una reina en (action, state[0])
-        newState = [state[0]]# copia de lista
+
+        newState = [state[0]] # copia de lista
         newState.append(state[1].copy())
         newState.append(state[2].copy())
         newState.append(state[3].copy())
@@ -42,6 +47,8 @@ class OchoReinas(Problem):
         return newState
 
     def goal_test(self, state):
+        """Comprueba si el estado es el objectivo"""
+        # Coste constante O(1)
         # PRE: State es valido, reinas no se comen entre si
         if state[0] == self.tam:# """and state[1] != [0, 6, 4, 7, 1, 3, 5, 2]"""   En todas las columnas hay una reina
             return True
